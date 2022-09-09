aa25984bdb7f09493bf666b294c58756bbaaad4f
Aoua1414 <107038532+Aoua1414@users.noreply.github.com>
2022-09-09 21:21:29 +0000

    Revert "Creatiob"

--- Free_tirage/src/app/app-routing.module.ts
+++ Free_tirage/src/app/app-routing.module.ts
@@ -1,32 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccueilComponent } from './accueil/accueil.component';
-import { ListeTireComponent } from './liste-tire/liste-tire.component';
-import { TirageComponent } from './tirage/tirage.component';
 
-const routes: Routes = [
-  {
-    path:"tirage",
-    component:TirageComponent
-  },
-  {
-    path:"listtire",
-    component:ListeTireComponent
-  },
-  {
-    path:"accueil",
-    component:AccueilComponent
-  },
-
-
-  {
-    path:"",
-    redirectTo:"accueil",
-    pathMatch:'full'
-  }
-  
-  
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
